75e6706d750167fef03b55af5c1c7467225c06b5
chelluoxou <chelluoxou@gmail.com>
2023-09-20 20:48:22 +0900

    breaking change: ルートのフォルダ構成の変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-import { Button } from "@chakra-ui/react";
-import Provider from "./Provider";
+'use client'
+import { Button } from '@chakra-ui/react'
+import Provider from './Provider'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { useAuthContext } from './AuthProvider'
 
-export default function Page () {
-  return (
-    <>
-      <Provider>
-        <Button colorScheme="blue">こんにちは</Button>
-      </Provider>
-    </>
-  )
+export default function Page() {
+  const user = useAuthContext()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (user === 'loading') return
+    if (user === null) router.push('/signin')
+    else router.push('/home')
+  }, [user])
+  return <></>
 }
