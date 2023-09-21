46b765b303e8d2458b6ac5a62678d2892c9a5a7f
chelluoxou <chelluoxou@gmail.com>
2023-09-21 14:13:04 +0900

    wip

---
+++ src/app/_utils/iot_music_box.ts
@@ -0,0 +1,56 @@
+type WordScoreMap = {
+  title: string
+  words: string[]
+  score: string
+}[]
+
+const wordScoreMap = [
+  {
+    title: '暴れん坊将軍',
+    words: ['暴れ'],
+    score:
+      'iT150 L24 C C L96 G L48 R L72 G# Eb F L128 G L96 R L12 F Eb D Eb L24 vG-RG ^C--R GF-Eb D--R C-Eb- ^Bb--C vG--- --RR vG-RG ^C--R EbD-Bb C--R G-FEb L36 DBb-vG L48 ^C--- ----',
+  },
+  {
+    title: '',
+    words: [],
+    score: '',
+  },
+  {
+    title: '',
+    words: [],
+    score: '',
+  },
+  {
+    title: '',
+    words: [],
+    score: '',
+  },
+  {
+    title: '',
+    words: [],
+    score: '',
+  },
+  {
+    title: '',
+    words: [],
+    score: '',
+  },
+  {
+    title: '',
+    words: [],
+    score: '',
+  },
+  {
+    title: '',
+    words: [],
+    score: '',
+  },
+  {
+    title: '',
+    words: [],
+    score: '',
+  },
+]
+
+export const attemptPlayFromPostBody = (postBody: string) => {}
